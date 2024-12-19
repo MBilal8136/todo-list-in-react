98ca6360e2d26bcd9630d6983f0892ea37cb5988
M Bilal <mbilal75362@gmail.com>
2024-12-19 13:34:30 -0800

    todo app

--- todo-list-in-react/src/compnents/Todo.jsx
+++ todo-list-in-react/src/compnents/Todo.jsx
@@ -2,19 +2,41 @@
 import "./Todo.css"
 import { useState } from "react"
 export default function Todo(){
-  const [Todos , setTodos] = useState(['simple'])
+  const [Todos , setTodos] = useState([''])
+  const [newValue, setNewValue] = useState("");
+
+  let addNewTask = ()=>{
+     setTodos([...Todos , newValue])
+  }
+
+  let updateTodoValue= (event)=>{
+    setNewValue(event.target.value)
+  }
+
+  
     return(
            <div>
-            <input type="text" placeholder="Add Tasks"  className="input"/><br /><br />
-            <button className="btn">ADD TASK</button>
+            <input
+              type="text"
+             placeholder="Add Tasks" 
+             className="input" 
+             value={newValue} 
+             onChange={updateTodoValue}
+             /><br /><br />
+            <button className="btn" onClick={addNewTask}>ADD TASK</button>
 
             <hr />
             <h4>Todo Tasks List</h4>
             
             <ul>
                 {Todos.map((todos)=>(
-                  <li>{todos}</li>
+                    <>
+                  <li className="lis">{todos} </li> 
+                 
+                   
+                   </>
                 ))}
+               
             </ul>
 
            </div>
